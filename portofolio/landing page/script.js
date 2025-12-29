33415f262745a0048ec7a84e4e3f7057bf635972
theodorus02 <148610599+theodorus02@users.noreply.github.com>
2025-12-30 01:10:04 +0700

    Delete portofolio directory

--- portofolio/landing page/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.card,.mockup,.price-card').forEach(el => {
-  el.style.transition = '.4s';
-  el.addEventListener('mouseover', () => {
-    el.style.transform = 'translateY(-10px)';
-  });
-  el.addEventListener('mouseout', () => {
-    el.style.transform = 'translateY(0)';
-  });
-});
